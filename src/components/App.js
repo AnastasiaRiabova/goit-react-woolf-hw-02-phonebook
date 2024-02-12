2ac3d4f249d1daebc05470f23d1bb00ad2509964
AnastasiaRiabova <riabova321@gmail.com>
2024-02-12 09:23:59 +0100

    toLowerCase

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,11 @@ class App extends Component {
   };
 
   addContact = ({ name, number }) => {
-    if (this.state.contacts.some(contact => contact.name === name)) {
+    if (
+      this.state.contacts.some(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
       alert(`${name} is already in contacts.`);
       return false;
     }
